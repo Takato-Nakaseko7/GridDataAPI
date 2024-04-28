23476ceec5b8632e51c7cbc5237177a91cc4435f
Takato-Nakaseko7 <116072222+Takato-Nakaseko7@users.noreply.github.com>
2024-04-28 14:11:45 -0500

    Add files via upload

--- GridDataAPI/GridDataAPI/GridDataController.cs
+++ GridDataAPI/GridDataAPI/GridDataController.cs
@@ -9,27 +9,41 @@ namespace GridDataAPI
 {
     public class GridDataController
     {
-        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        private SqlConnectionStringBuilder builder { get;  set; }
         
         public GridDataController()
         {
-            builder.DataSource = "DESKTOP-HG8TVFE\\SQLEXPRESS";
+            builder = new SqlConnectionStringBuilder();
+
+            // When testing on other machine, change properties below to mactch your environment.
+            builder.DataSource = "DESKTOP-HG8TVFE\\SQLEXPRESS"; 
             builder.UserID = "sa";
             builder.Password = "sa";
             builder.InitialCatalog = "grid_data";
         }
 
-
+        /// <summary>
+        /// Get the latest value within the given time range
+        /// </summary>
+        /// <param name="startTime">
+        /// Start datetime
+        /// </param>
+        /// <param name="endTime">
+        /// End datetime
+        /// </param>
+        /// <returns>
+        /// The latest value within the two given datetime. When failed, return null.
+        /// </returns>
         public string? GetLatestValue(DateTime startTime, DateTime endTime)
         {
-            string query = $"Select\r\n\ttop 1 value\r\n  from \r\n\tMeasure\r\n  where\r\n\trecord_time >= '{startTime}' and record_time <= '{endTime}'\r\n  order by\r\n\trecord_time desc";
+            // SQL Query
+            string query = $"select\r\n\ttop 1 value\r\n  from \r\n\tMeasure\r\n  where\r\n\trecord_time >= '{startTime}' and record_time <= '{endTime}'\r\n  order by\r\n\trecord_time desc";
 
             try
             {
                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
                 {
                     connection.Open();
-                    Console.WriteLine("Connection succeeded");
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
                         using (SqlDataReader reader = command.ExecuteReader())
@@ -49,5 +63,55 @@ namespace GridDataAPI
             
         }
 
+        /// <summary>
+        /// Get a list of values corresponding to the collected time and between start and end time.
+        /// </summary>
+        /// <param name="startTime">
+        /// Start datetime
+        /// </param>
+        /// <param name="endTime">
+        /// End datetime
+        /// </param>
+        /// <param name="collectedTime">
+        /// Collected datetime
+        /// </param>
+        /// <returns>
+        /// A list of values corresponding to the collected time and between start time and end time. When failed, return null.
+        /// </returns>
+        public List<string>? GetCollectedData(DateTime startTime, DateTime endTime, DateTime collectedTime)
+        {
+            // SQL Query
+            string query = $"select \r\n\tnode_id, value, record_time\r\n  from \r\n\tMeasure\r\n  where\r\n\trecord_time >= '{startTime}' and record_time <= '{endTime}'\r\n    and\r\n\ttarget_time = '{collectedTime}'";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader dr = command.ExecuteReader())
+                        {
+                            List<string> values = new List<string>();
+
+                            // While there is data, keep adding to the list that will be returned
+                            while (dr.Read())
+                            {
+                                if (!string.IsNullOrEmpty(dr["value"].ToString()))
+                                {
+                                    values.Add(dr["value"].ToString());
+                                }
+                            }
+                            return values;
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
     }
 }
--- GridDataAPI/GridDataAPI/Program.cs
+++ GridDataAPI/GridDataAPI/Program.cs
@@ -1,13 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GridDataAPI;
+using System.Data.SqlClient;
 
 GridDataController gridDataController = new GridDataController();
 
-DateTime startTime = DateTime.Parse("2024/04/27 00:00");
-DateTime endTime = DateTime.Parse("2024/04/27 05:00");
+// Change Datetime to test different result.
+DateTime startTime = DateTime.Parse("2024/04/27 20:00");
+DateTime endTime = DateTime.Parse("2024/04/27 23:00");
+DateTime collectedTime = DateTime.Parse("2024/04/28 23:00");
 
+// Result of API that will accept a start datetime and end datetime. The API should return the latest value for each timestamp in the date range. 
 string? latestValue = gridDataController.GetLatestValue(startTime, endTime);
 
-Console.WriteLine(latestValue);
+Console.WriteLine($"The latest value between {startTime.ToString("yyyy/MM/dd HH:mm:ss")} and {endTime.ToString("yyyy/MM/dd HH:mm:ss")} is {latestValue}");
 
+// Result of API that will accept a start datetime, end datetime and collected datetime. The API should return the value corresponding to the collected datetime for each timestamp in the date range. 
+List<string>? values = gridDataController.GetCollectedData(startTime, endTime, collectedTime);
 
+Console.WriteLine("The Values collected are");
+
+foreach (string value in values)
+{
+    Console.WriteLine(value);
+}
